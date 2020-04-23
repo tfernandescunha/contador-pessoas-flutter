159d7abc0d5aa0523c2491571b2514cc069100c6
Thiago <tfernandes.cunha@gmail.com>
2020-04-23 16:18:54 -0300

    Contador de pessoas - flutter - primeiro projeto

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:contadordepessoas/widgets/home.widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: 'Contador de Pessoas', home: Home()));
+}
